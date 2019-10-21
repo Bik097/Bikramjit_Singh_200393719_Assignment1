0e671c86e3bd804e9ca7c6a4e103ebc1c3edfa28
Bik097 <bjs241997@gmail.com>
2019-10-21 01:26:08 -0400

    about me basic

--- Bikramjit_Singh_200393719_Assignment1/routes/about.js
+++ Bikramjit_Singh_200393719_Assignment1/routes/about.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' , portfolio: 'Bik'});
+    res.render('about', { title: 'Express' , portfolio: "Bik's profile"});
 });
 
 module.exports = router;
